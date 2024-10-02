d21bc5e7bd56cc543cc4017c2cf5d400bc9e19b0
Ksum7 <xamchik02@gmail.com>
2024-10-02 19:38:31 +0300

    update prettier

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT);
-  console.log(`Application is running on: http://localhost:${process.env.PORT}`);
+  console.log(
+    `Application is running on: http://localhost:${process.env.PORT}`
+  );
 }
 bootstrap();
