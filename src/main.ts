ab5215580716ddb274586b107ec2db005af9751a
Ksum7 <xamchik02@gmail.com>
2025-05-11 18:35:31 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: false });
+    const app = await NestFactory.create(AppModule, {
+        cors: {
+            origin: 'https://each-job.ru:8443',
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+            credentials: true,
+        },
+    });
 
     const configService = app.get(ConfigService);
     const port = configService.get<number>('PORT', 3000);
